c8a9a9d551e7b1f55d57ac3d911c055337a69cfd
Karlos Silva <karlossilva194@gmail.com>
2021-05-27 16:26:04 -0300

    Add typescript and add global styles using sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
