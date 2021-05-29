25430e0593ea6533f64db14f89cdd07ea613dae9
Karlos Silva <karlossilva194@gmail.com>
2021-05-29 16:30:08 -0300

    Create Homepage and Component to Button Subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Codding" />
+      </main>
     </>
   )
 }
