84f75ce0c3f4a378140501791fcc6f372193cf00
Karlos Silva <karlossilva194@gmail.com>
2021-07-09 12:17:40 -0300

    Make conecction with database faunaDb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,5 +12,39 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
 
+      try {
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+
+    },
+  }
 })
